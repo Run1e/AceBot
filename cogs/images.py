4fe9b4fa15ee724ce9b4cee841d9da33186c2bcb
Run1e <runar-borge@hotmail.com>
2019-08-20 14:04:13 +0200

    animal commands falls back to fs upon query failure

--- cogs/images.py
+++ cogs/images.py
@@ -1,8 +1,10 @@
 import discord
 import asyncio
+import aiohttp
 import os
 import logging
 import io
+import random
 
 from discord.ext import commands
 
@@ -60,15 +62,11 @@ class ImagePersister:
 		return data
 
 	async def request(self, url, method, params=None):
-		try:
-			async with self.aiohttp.get(url, params=params) as resp:
-				if resp.status != 200:
-					raise QUERY_ERROR
+		async with self.aiohttp.get(url, params=params) as resp:
+			if resp.status != 200:
+				raise QUERY_ERROR
 
-				return await getattr(resp, method)()
-
-		except asyncio.TimeoutError:
-			raise QUERY_ERROR
+			return await getattr(resp, method)()
 
 	def fetch_if_exist(self, name):
 		file = f'{self.path}/{name}'
@@ -83,9 +81,19 @@ class ImagePersister:
 		with open(f'{self.path}/{name}', 'wb') as f:
 			f.write(data)
 
+	def get_random_image(self):
+		try:
+			file = random.choice(os.listdir(self.path))
+		except IndexError:
+			return None, None
+		return file, self.fetch_if_exist(file)
+
 
 class Image(AceMixin, commands.Cog):
 	''':heart_eyes: :heart_eyes: :heart_eyes:'''
+
+	QUERY_EXCEPTIONS = (aiohttp.ClientError, asyncio.TimeoutError, FileTooLargeError)
+	EXCEPTION_FORMAT = 'Resorting to file system cache: {}'
 
 	def __init__(self, bot):
 		super().__init__(bot)
@@ -104,22 +112,25 @@ class Image(AceMixin, commands.Cog):
 		url = 'https://random.dog/'
 		params = dict(filter='mp4')
 
+		file = None
+		data = None
+
 		async with ctx.typing():
-			for attempt in range(MAX_ATTEMPTS):
-				try:
-					id = await self.woof_persister.request_text(url + 'woof', params)
-					data = self.woof_persister.fetch_if_exist(id)
+			try:
+				file = await self.woof_persister.request_text(url + 'woof', params)
+				data = self.woof_persister.fetch_if_exist(file)
 
-					if data is None:
-						data = await self.woof_persister.request_data(url + id)
-						self.woof_persister.store_image(id, data)
-
-					await ctx.send(file=discord.File(io.BytesIO(data), id))
-					return
-				except FileTooLargeError:
-					continue
-
-		raise QUERY_ERROR
+				if data is None:
+					data = await self.woof_persister.request_data(url + file)
+					self.woof_persister.store_image(file, data)
+			except self.QUERY_EXCEPTIONS as exc:
+				log.warning(self.EXCEPTION_FORMAT.format(str(exc)))
+				file, data = self.woof_persister.get_random_image()
+			finally:
+				if data is not None:
+					await ctx.send(file=discord.File(io.BytesIO(data), file))
+				else:
+					raise QUERY_ERROR
 
 	@commands.command(aliases=['cat'])
 	@commands.bot_has_permissions(attach_files=True)
@@ -133,26 +144,30 @@ class Image(AceMixin, commands.Cog):
 		url = 'https://api.thecatapi.com/v1/images/search'
 		params = dict(api_key=THECATAPI_KEY)
 
+		file = None
+		data = None
+
 		async with ctx.typing():
-			for attempt in range(MAX_ATTEMPTS):
-				try:
-					json = await self.meow_persister.request_json(url, params)
+			try:
+				json = await self.meow_persister.request_json(url, params)
 
-					img_url = json[0]['url']
-					file = img_url.split('/')[-1]
+				img_url = json[0]['url']
+				file = img_url.split('/')[-1]
 
-					data = self.meow_persister.fetch_if_exist(file)
+				data = self.meow_persister.fetch_if_exist(file)
 
-					if data is None:
-						data = await self.meow_persister.request_data(img_url)
-						self.meow_persister.store_image(file, data)
+				if data is None:
+					data = await self.meow_persister.request_data(img_url)
+					self.meow_persister.store_image(file, data)
 
+			except self.QUERY_EXCEPTIONS as exc:
+				log.warning(self.EXCEPTION_FORMAT.format(str(exc)))
+				file, data = self.meow_persister.get_random_image()
+			finally:
+				if data is not None:
 					await ctx.send(file=discord.File(io.BytesIO(data), file))
-					return
-				except FileTooLargeError:
-					continue
-
-		raise QUERY_ERROR
+				else:
+					raise QUERY_ERROR
 
 	@commands.command(aliases=['duck'])
 	@commands.bot_has_permissions(attach_files=True)
@@ -162,27 +177,29 @@ class Image(AceMixin, commands.Cog):
 
 		url = 'https://random-d.uk/api/v1/random'
 
+		file = None
+		data = None
+
 		async with ctx.typing():
-			for attempt in range(MAX_ATTEMPTS):
-				try:
-					json = await self.quack_persister.request_json(url)
+			try:
+				json = await self.quack_persister.request_json(url)
 
-					img_url = json['url']
-					file = img_url.split('/')[-1]
+				img_url = json['url']
+				file = img_url.split('/')[-1]
 
-					data = self.quack_persister.fetch_if_exist(file)
+				data = self.quack_persister.fetch_if_exist(file)
 
-					if data is None:
-						data = await self.quack_persister.request_data(img_url)
-						self.quack_persister.store_image(file, data)
-
+				if data is None:
+					data = await self.quack_persister.request_data(img_url)
+					self.quack_persister.store_image(file, data)
+			except self.QUERY_EXCEPTIONS as exc:
+				log.warning(self.EXCEPTION_FORMAT.format(str(exc)))
+				file, data = self.quack_persister.get_random_image()
+			finally:
+				if data is not None:
 					await ctx.send(file=discord.File(io.BytesIO(data), file))
-					return
-				except FileTooLargeError:
-					continue
-
-		raise QUERY_ERROR
-
+				else:
+					raise QUERY_ERROR
 
 	@commands.command(aliases=['fox'])
 	@commands.bot_has_permissions(attach_files=True)
@@ -192,26 +209,29 @@ class Image(AceMixin, commands.Cog):
 
 		url = 'https://randomfox.ca/floof/'
 
+		file = None
+		data = None
+
 		async with ctx.typing():
-			for attempt in range(MAX_ATTEMPTS):
-				try:
-					json = await self.floof_persister.request_json(url)
+			try:
+				json = await self.floof_persister.request_json(url)
 
-					img_url = json['image']
-					file = img_url.split('/')[-1]
+				img_url = json['image']
+				file = img_url.split('/')[-1]
 
-					data = self.floof_persister.fetch_if_exist(file)
+				data = self.floof_persister.fetch_if_exist(file)
 
-					if data is None:
-						data = await self.floof_persister.request_data(img_url)
-						self.floof_persister.store_image(file, data)
-
+				if data is None:
+					data = await self.floof_persister.request_data(img_url)
+					self.floof_persister.store_image(file, data)
+			except self.QUERY_EXCEPTIONS as exc:
+				log.warning(self.EXCEPTION_FORMAT.format(str(exc)))
+				file, data = self.floof_persister.get_random_image()
+			finally:
+				if data is not None:
 					await ctx.send(file=discord.File(io.BytesIO(data), file))
-					return
-				except FileTooLargeError:
-					continue
-
-		raise QUERY_ERROR
+				else:
+					raise QUERY_ERROR
 
 
 def setup(bot):
