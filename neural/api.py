6ab0981aa285956b298310eabbe3f18c0104d366
runie <runar-borge@hotmail.com>
2021-06-16 18:52:35 +0200

    Finish basic API

--- neural/api.py
+++ neural/api.py
@@ -1,7 +1,33 @@
+import pickle
+
+import torch
 from sanic import Request, Sanic
 from sanic.response import HTTPResponse, json
+from torchtext.data.utils import get_tokenizer
+
+from model import TextCNN
+from text_processor import TextProcessor
+from torch_config import EMBEDDINGS_DIR
 
 app = Sanic('PyTorch API')
+
+embeddings = torch.load(f'{EMBEDDINGS_DIR}/vectors.pkl')
+
+model = TextCNN(
+	embeddings=embeddings,
+	n_filters=64,
+	filter_sizes=[2, 3],
+	dropout=0.0,
+)
+
+model.load_state_dict(torch.load('model_state.pth'))
+model.eval()
+
+text_processing = TextProcessor(
+	wti=pickle.load(open(f'{EMBEDDINGS_DIR}/wti.pkl', 'rb')),
+	tokenizer=get_tokenizer('basic_english'),
+	standardize=True,
+)
 
 
 @app.post('/game')
@@ -11,7 +37,13 @@ async def game(request: Request):
 	if q is None:
 		return HTTPResponse(status=400)
 
-	return json(dict(x='a'))
+	tokens = text_processing.process(q)
+	x = torch.unsqueeze(tokens, dim=0)
+
+	pred = model(x)
+	pred = torch.squeeze(pred).item()
+
+	return json(dict(p=pred))
 
 
 app.run('localhost', 80)
