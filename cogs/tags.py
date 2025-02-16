288f80421a0aadb6c6ddb06826dd857d460e4a19
runie <runie@runie.io>
2025-02-16 08:14:37 +0100

    change tag delete button to make less distracting

--- cogs/tags.py
+++ cogs/tags.py
@@ -274,10 +274,9 @@ class Tags(AceMixin, commands.Cog):
         _, record = await TagViewConverter().convert(inter, query.split(ZWS)[0])
 
         ar = disnake.ui.ActionRow()
-        ar.add_button(0, style=disnake.ButtonStyle.danger, label="🗑️", custom_id=f"tagsdeletebutton_{inter.author.id}")
-        msg = await inter.send(record.get("content"), allowed_mentions=disnake.AllowedMentions.none(), components=[ar])
+        ar.add_button(0, style=disnake.ButtonStyle.secondary, label="🗑️", custom_id=f"tagsdeletebutton_{inter.author.id}")
 
-        msg = await inter.original_message()
+        await inter.send(record.get("content"), allowed_mentions=disnake.AllowedMentions.none(), components=[ar])
 
         await self.db.execute(
             "UPDATE tag SET uses=$2, viewed_at=$3 WHERE id=$1",
@@ -306,7 +305,7 @@ class Tags(AceMixin, commands.Cog):
 
         tag_name, record = tag_name
         ar = disnake.ui.ActionRow()
-        ar.add_button(0, style=disnake.ButtonStyle.danger, label="🗑️", custom_id=f"tagsdeletebutton_{ctx.author.id}")
+        ar.add_button(0, style=disnake.ButtonStyle.secondary, label="🗑️", custom_id=f"tagsdeletebutton_{ctx.author.id}")
         await ctx.send(record.get("content"), allowed_mentions=disnake.AllowedMentions.none(), components=[ar])
 
         await self.db.execute(
